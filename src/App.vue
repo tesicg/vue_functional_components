701940e5262eb75491bfa78f07b12239a318080e
Тешич Горан <goranvostok@gmail.com>
2020-11-06 06:33:41 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <FunctionalCars
+     :cars = "['BMW', 'Volvo', 'Opel', 'Audi']"
+    />
+  </div>
+</template>
+
+<script>
+import FunctionalCars from './components/FunctionalCars.vue'
+
+export default {
+  name: 'app',
+  components: {
+    FunctionalCars
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/FunctionalCars.vue
@@ -0,0 +1,18 @@
+<template functional>
+  <div>
+    <!-- <p v-for="car in props.cars" :key="car">{{car}}</p> -->
+    <ul v-for="car in props.cars" :key="car">
+      <li>{{car}}</li>
+    </ul>
+  </div>
+</template>
+
+<script> 
+export default {
+  functional: true,
+  name: 'functionalCars',
+  props: {
+    cars: Array
+  }
+}
+</script>
